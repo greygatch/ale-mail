d1827f9debdab067b065b520b739e692ab1e9ac7
Evan Gatchell <greygatch@gmail.com>
2015-07-25 20:21:44 -0700

    express routes

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 var express = require('express')
-  , app = express();
+  , app = express()
+  , BreweryDb = require("brewerydb-node")
+  , brewdb = new BreweryDb('cd8bebc7e8174e11971635b9de526b79');
 
 app.engine('jade', require('jade').__express)
 app.set('view engine', 'jade')
 
 app.use(express.static(__dirname + '/public'))
+
+app.get('/beer/:name', function(req, res){
+
+  var returnData;
+
+  brewdb.beer.find({ withBreweries: "N", name: req.params.name }, function(err, response){
+    if(err){ res.status(400); }
+
+    if(!response){
+      brewdb.beer.find({ withBreweries: "N", name: req.params.name + '*' }, function(err, data){
+        returnData = data;
+        res.json(returnData);
+      });
+    }
+    if (response){
+      res.json(response)
+    }
+  });
+})
 
 app.get('/', function(req, res){
   res.render('index')
